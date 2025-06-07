29d187337d2d90363106d783c21d37b39cdf3cfc
Gabrielle Stieler <gabriellestieler17@gmail.com>
2025-06-07 18:54:30 -0300

    Ajuste de inicial de classes

---
+++ src/main/java/testes/TesteCriaTabela.java
@@ -0,0 +1,37 @@
+package testes;
+
+import jakarta.persistence.*;
+//import models.EstoqueRoupa;
+import models.Usuario;
+
+public class TesteCriaTabela {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("taskhubPU");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Usuario u = new Usuario();
+        u.setNome("Gabrielle");
+        u.setEmail("gabriellestieler@gmail.com");
+        u.setSenha("1234567");
+
+        em.persist(u);
+
+//        EstoqueRoupa er = new EstoqueRoupa();
+//        er.setNomePeca("Camisa Polo");
+//        er.setQuantidade(10);
+//        er.setPrecoUnitario(25.99);
+//        er.setCategoria("Camisa");
+//        er.setCor("Preto");
+//        er.setTamanho("P");
+//
+//        em.persist(er);
+
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+
+        System.out.println("Item salvo com sucesso!");
+    }
+}
